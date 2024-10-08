9b6fd6c50e3df3fd8dc250d1ea6d08e67d0a861f
JoshuaDeal02 <joshuadeal02@gmail.com>
2024-10-08 16:15:22 -0700

    Add files via upload

---
+++ PythonBoto3IAMScripts/CreateRole.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+
+def create_role():
+    iam = boto3.client('iam')
+    role_name = 'newRole'
+    #The trust policy document is an example, the user could input a different json file instead for actual use
+    trust_policy = {
+        "Version":"2012-10-17",
+        "Statement":[
+            {
+                "Effect":"Allow",
+                "Principal":{
+                    "Service":"ec2.amazonaws.com"
+                },
+                "Action":"sts:AssumeRole"
+            }
+        ]
+
+    }
+    iam.create_role(RoleName = role_name, AssumeRolePolicyDocument = json.dumps(trust_policy))
+
+create_role()
